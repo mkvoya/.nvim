7471ee801bb6da8534698d2b52b7ffc566899d52
Mingkai Dong <mk@dong.mk>
2024-05-07 12:08:38 +0800

    update to the latest

---
+++ lua/config/gui.lua
@@ -0,0 +1,5 @@
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  -- <D-v>
+  vim.o.guifont = "Monaco:h12,LXGW WenKai Screen:h12,Sarasa Term SC Nerd:h12"
+end
--- lua/plugins/input.lua
+++ lua/plugins/input.lua
@@ -1,3 +1,7 @@
 return {
   { "noearc/jieba.nvim", dependencies = { "noearc/jieba-lua" } },
+  {
+    "zzhirong/vim-easymotion-zh",
+    dependencies = { "easymotion/vim-easymotion" },
+  },
 }
